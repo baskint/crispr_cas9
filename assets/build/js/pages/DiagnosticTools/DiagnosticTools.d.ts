4e09ca8f4f500cb2b9af3a33b67d8bbfb89c1605
Baskin Tapkan <baskint@gmail.com>
2021-10-31 00:27:28 -0500

    version updates

--- assets/build/js/pages/DiagnosticTools/DiagnosticTools.d.ts
+++ assets/build/js/pages/DiagnosticTools/DiagnosticTools.d.ts
@@ -1,3 +1,2 @@
-/// <reference types="react" />
 declare const DiagnosticTools: () => JSX.Element;
 export default DiagnosticTools;
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Crispr.MixProject do
     [
       app: :crispr,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -33,16 +33,16 @@ defmodule Crispr.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.9"},
+      {:phoenix, "~> 1.5.13"},
       {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_reload, "~> 1.3.3", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:absinthe, "~> 1.5"},
+      {:absinthe, "~> 1.6"},
       {:absinthe_plug, "~> 1.5"},
     ]
   end
