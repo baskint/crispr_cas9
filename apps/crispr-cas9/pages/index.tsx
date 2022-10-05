9c391a55df79d0aa68df345fba5f52df2c213ab7
Baskin Tapkan <baskint@gmail.com>
2022-10-05 15:43:14 -0500

    restart

--- apps/crispr-cas9/pages/index.tsx
+++ apps/crispr-cas9/pages/index.tsx
@@ -1,11 +1,18 @@
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  .page {
+  }
+`;
+
 export function Index() {
   /*
    * Replace the elements below with your own.
    *
-   * Note: The corresponding styles are in the ./index.styled-jsx file.
+   * Note: The corresponding styles are in the ./index.styled-components file.
    */
   return (
-    <div>
+    <StyledPage>
       <div className="wrapper">
         <div className="container">
           <div id="welcome">
@@ -406,7 +413,7 @@ export function Index() {
           </p>
         </div>
       </div>
-    </div>
+    </StyledPage>
   );
 }
 
