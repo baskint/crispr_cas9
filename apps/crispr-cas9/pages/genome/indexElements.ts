99ad2c4519de40361145b072172f19bcc2c8bfb5
Baskin Tapkan <baskint@gmail.com>
2022-10-09 23:16:40 -0500

    cas9

--- apps/crispr-cas9/pages/genome/indexElements.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { Accordion } from '@mui/material/core';
-
-const Cas9AccordionWrapper = styled('Accordion')({
-  backgroundColor: #efefef;
-})
-
-
-export {Cas9AccordionWrapper};
