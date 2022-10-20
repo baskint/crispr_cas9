a7c4f7c04c0e560191fc0fa19fa31cb32b2e85d6
Baskin Tapkan <baskint@gmail.com>
2022-10-19 21:57:39 -0500

    updates to prod

--- apps/crispr-cas9/components/cas9elements.ts
+++ apps/crispr-cas9/components/cas9elements.ts
@@ -2,32 +2,32 @@ import styled from 'styled-components';
 import {
   Accordion,
   AccordionSummary,
-  AccordionDetails,
+  // AccordionDetails,
   Typography,
 } from '@mui/material';
 import { ExpandCircleDownOutlined } from '@mui/icons-material';
 
 const StyledAccordion = styled(Accordion)`
-  background-color: hsla(214, 62%, 21%, 1);
-  color: white;
-  border-radius: 16px;
   &.MuiPaper-root {
+    background-color: hsla(214, 62%, 21%, 1);
+    color: white;
+    border-radius: 16px;
     border-top-left-radius: 16px;
     border-top-right-radius: 16px;
   }
   &.MuiAccordion-root {
     border-radius: 16px;
   }
-`
+`;
 
 const StyledAccordionSummary = styled(AccordionSummary)`
   border-radius: 16px;
+`;
 
-`
 const StyledHeader = styled(Typography)`
   &.MuiTypography-root {
-   text-align: center;
-   width: 100%;
+    text-align: center;
+    width: 100%;
   }
 `;
 
